da0050e2df00a94c1541093c67fbd4e1c47fed3b
RohakDev <45899405+ImRohak@users.noreply.github.com>
2023-01-18 18:09:25 +0000

    Created project basics

---
+++ app.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from pypresence import Presence
+import time
+
+root = tk.Tk()
+
+root.title("Premid 2.0")
+
+message = tk.Label(root, text="Presence running")
+message.pack()
+
+client_id = "1065329007693942864"
+RPC = Presence(client_id)
+
+RPC.connect()
+
+RPC.update(state="Making my own premid") # Updates our presence
+
+print("Presence online")
+
+root.mainloop()
